f7395e24096f0a713331412596345dbb8367565e
thamaraikannan <thamaraik69@gmail.com>
2024-03-31 12:35:19 +0530

    first push one

---
+++ src/container/education/form.tsx
@@ -0,0 +1,212 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { css } from "@emotion/react";
+import { ClipLoader } from "react-spinners";
+import { CourseContent } from "../../../utils/CourseContent";
+import axios from "axios";
+
+const CourseForm = () => {
+  const [showForm, setShowForm] = useState(true);
+  const [responseMessage, setResponseMessage] = useState("");
+  const [Name,setName] = useState<string>();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      qualification: "",
+      center_name: "",
+      course_name: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name is required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+      mobile: Yup.string()
+        .matches(/^[0-9]+$/, "Invalid phone number") // Only allow digits
+        .min(10, "Must be at least 10 characters")
+        .required("Mobile is required"),
+      qualification: Yup.string().required("Qualification is required"),
+      center_name: Yup.string().required("Location is required"),
+    }),
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      setName(values.name)
+      axios.post('https://anjalicomputereducation.com/api/?key=91d671f835b9d92970ba5460e8be0dc2d6c49356&req_data=enq', values)
+        .then((response : any) => {
+          if (response.status === 200) {
+            setShowForm(false);
+            setResponseMessage(response.data);
+          } else if (response.status === 201) {
+            setResponseMessage("Error: " + response.data.msg);
+          }
+          setSubmitting(false);
+          resetForm();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  });
+
+  const centreArray = [
+    {
+      id: 1,
+      name: "Tambaram",
+      ccode: "anjali01",
+    },
+    {
+      id: 2,
+      name: "Selaiyur",
+      ccode: "anjali02",
+    },
+    {
+      id: 3,
+      name: " Chengalpattu",
+      ccode: "anjali03",
+    },
+    {
+      id: 4,
+      name: "Guduvancheery",
+      ccode: "anjali04",
+    },
+  ];
+  console.log(responseMessage)
+
+  return (
+    <div className="border bg-white border-green-500 p-5 w-[100%] m-auto rounded-lg shadow-md shadow-green-500">
+       {showForm ? (
+        <>
+         <p className="lg:text-2xl text-xl mb-4 font-semibold">Enquiry Now </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="w-[100%] mb-2">
+          <label className="text-[16px] font-semibold">Course</label>
+          <br />
+          <select name="course_name" id="" className="shadowBox w-[100%] border py-1.5 mt-1 rounded px-2"
+          >
+            <option value="">Please Select Course</option>
+            {
+                CourseContent.map((values)=>(
+                    <option className={values.courseId}>{values.courseTitle}</option>
+                ))
+            }
+          </select>
+        </div>
+        <div className="w-[100%]">
+          <label className="text-[16px] font-semibold" htmlFor="name">
+            Name
+          </label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            className="shadowBox w-[100%] border py-1.5 mt-1 rounded px-2"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div className="text-red-500 text-sm">{formik.errors.name}</div>
+          ) : null}
+        </div>
+        <div className="w-[100%] mt-2">
+          <label className="text-[16px] font-semibold" htmlFor="email">
+            Email
+          </label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            className="shadowBox w-[100%] border py-1.5 mt-1 rounded px-2"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-red-500 text-sm">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className="w-[100%] mt-2">
+          <label className="text-[16px] font-semibold" htmlFor="mobile">
+            Mobile
+          </label>
+          <br />
+          <input
+            type="text"
+            name="mobile"
+            className="shadowBox w-[100%] border py-1.5 mt-1 rounded px-2"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.mobile}
+          />
+          {formik.touched.mobile && formik.errors.mobile ? (
+            <div className="text-red-500 text-sm">{formik.errors.mobile}</div>
+          ) : null}
+        </div>
+        <div className="w-[100%] mt-2">
+          <label className="text-[16px] font-semibold" htmlFor="qualification">
+            Qualification
+          </label>
+          <br />
+          <input
+            type="text"
+            name="qualification"
+            className="shadowBox w-[100%] border py-1.5 mt-1 rounded px-2"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.qualification}
+          />
+          {formik.touched.qualification && formik.errors.qualification ? (
+            <div className="text-red-500 text-sm">
+              {formik.errors.qualification}
+            </div>
+          ) : null}
+        </div>
+        <div className="w-[100%] mt-2">
+          <label className="text-[16px] font-semibold" htmlFor="center_name">
+            Nearby Location
+          </label>
+          <br />
+
+          <select
+            name="center_name"
+            className="shadowBox w-[100%] border py-1.5 mt-1 rounded px-2"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.center_name}
+          >
+            <option value="">Please Select </option>
+            {centreArray.map((values) => (
+              <option value={values.ccode}>{values.name}</option>
+            ))}
+          </select>
+          {formik.touched.center_name && formik.errors.center_name ? (
+            <div className="text-red-500 text-sm">
+              {formik.errors.center_name}
+            </div>
+          ) : null}
+        </div>
+        <div className="mt-2 flex justify-center">
+          <button
+            type="submit"
+            className="px-6 py-2 border font-semibold bg-blue-600 text-white rounded"
+            disabled={formik.isSubmitting}
+          >
+            {formik.isSubmitting ? (
+              <ClipLoader color="#fff" size={15} />
+            ) : (
+              "Submit"
+            )}
+          </button> 
+        </div>
+      </form>
+        </>
+       ) : <div>Hello <span className="font-semibold text-green-500"> {Name} </span>, be ready! We will contact you very shortly.
+       </div> }
+     
+    </div>
+  );
+};
+
+export default CourseForm;
--- src/pages/education/[education].js
+++ src/pages/education/[education].js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios"; // Import axios
 import Head from "next/head";
 import { KeywordArray, CenterDetails } from "../../../utils/center";
+import CourseForm from "@/container/education/form";
+import Image from "next/image";
+import InfiniteLooper from "../../container/Infinity"
 
 export const getStaticPaths = async (context) => {
   try {
@@ -58,8 +61,7 @@ export const getStaticProps = async ({ params }) => {
   } catch (error) {
     console.error("Error fetching data:", error);
     return {
-      // Return an object even in case of error
-      props: { coursedata: null }, // Set coursedata to null or handle the error appropriately
+      props: { coursedata: null }, 
     };
   }
 };
@@ -76,16 +78,157 @@ function CenterName({ params }) {
     );
   }, []);
 
+  const CompanyLogo = [
+    {
+      id: 1,
+      campany: "/company/7611770.png",
+      comapanyName: "google",
+    },
+    {
+      id: 2,
+      campany: "/company/CGI_Inc.-Logo.wine.png",
+      comapanyName: "CGI_Inc",
+    },
+    {
+      id: 3,
+      campany: "/company/cisco_logo-1000px.png",
+      comapanyName: "cisco_logo",
+    },
+    {
+      id: 4,
+      campany: "/company/Cognizant_logo_2022.svg.png",
+      comapanyName: "Cognizant_logo",
+    },
+    {
+      id: 5,
+      campany: "/company/Dell_logo.png",
+      comapanyName: "Dell_logo",
+    },
+    {
+      id: 6,
+      campany: "/company/hp-logo.com.png",
+      comapanyName: "hp-logo",
+    },
+    {
+      id: 7,
+      campany: "/company/IBM_logo.svg.png",
+      comapanyName: "IBM_logo",
+    },
+    {
+      id: 8,
+      campany: "/company/Infosys_logo.svg.png",
+      comapanyName: "Infosys_logo",
+    },
+    {
+      id: 9,
+      campany: "/company/Intel-logo-2022.png",
+      comapanyName: "Intel",
+    },
+    {
+      id: 10,
+      campany:
+        "/company/png-transparent-microsoft-logo-microsoft-thumbnail.png",
+      comapanyName: "microsoft",
+    },
+    {
+      id: 11,
+      campany: "/company/sutherland-global-services.png",
+      comapanyName: "sutherland",
+    },
+    {
+      id: 12,
+      campany: "/company/Tata_Consultancy_Services_Logo.svg.png",
+      comapanyName: "Tata_Consultancy_Services_Logo",
+    },
+  ];
+
+
   console.log(Title);
   return (
     <>
       <Head>
         <title>{Title}</title>
       </Head>
-      <div>
-        <div>
-          <h1 className="text-center font-semibold capitalize ">{Title}</h1>
+      <div className="gradient_one">
+      <div className="max-w-7xl m-auto ">
+      <div className=" px-16 py-2">
+        <div className="lg:flex gap-24 justify-center items-center">
+        <div className="w-[65%] m-auto">
+        <div className=" bg-white py-4 rounded-lg shadow-lg">
+            <h1 className=" font-extrabold capitalize text-[36px] flex justify-center items-center w-[60%] m-auto text-slate=400 antialiased">{Title}</h1>
+          </div>
+        <h2 className="text-black text-[26px] text-center mt-6 font-semibold antialiased">A unit of Anjali education & Charitable trust</h2>
+        <h3 className="text-center mt-4 text-[20px] font-semibold text-slate-500 w-[70%] m-auto antialiased">Licenced from ministry of corporate affairs - Govt. of India</h3>
         </div>
+         
+          <div className="w-[35%]"><CourseForm/></div>
+        </div>
+      </div>
+     
+      </div>
+      </div>
+      
+      <div className=" py-6 ">
+      <div className="lg:max-w-7xl m-auto  ">
+      <div className="py-10">
+      <div className="lg:flex gap-10">
+      <div className="lg:w-[50%] p-8 border bg-white rounded-lg">
+        <p className="text-[18px] leading-8 antialiased">At Anjali Computer Education, we specialize in delivering top-notch training in both computer software and soft skills. Our programs cater to students, working professionals, and individuals seeking employment opportunities. With a team of highly skilled industry experts, we ensure that our students receive the best guidance to develop and enhance their skills. Whether it's mastering software applications or refining essential soft skills, we are committed to providing world-class education that empowers individuals to succeed in today's competitive landscape. Join us to unlock your full potential and thrive in the dynamic world of technology and professional development.</p>
+
+      <div className="mt-10">
+        <ol>
+        <li>World-Class Industry Experts:</li>
+        <li>Hands-On Practical Experience:</li>
+        <li>Online &amp; Offline Classes:</li>
+        <li>Flexible Timing:</li>
+        <li>Easy Installment Plans:</li>
+        <li>Part-Time Job Opportunities:</li>
+        <li>High-Tech Computer Labs:</li>
+        <li>Regular &amp; Weekend Classes:</li>
+        <li>Special Timing for Students &amp; Housewives:</li>
+        <li>ISO Certified Courses:</li>
+        </ol>
+      </div>
+
+        <p className="gradient_one text-center md:text-[30px] text-[20px] font-semibold lg:mt-[80px] mt-20 mb-[60px] antialiased  ">
+          Get chances to work at top companies
+        </p>
+
+        <InfiniteLooper speed={400} direction="left">
+            <div className="flex gap-24 bg-gray-100 mb-5">
+              {CompanyLogo.map((values) => (
+                <Image
+                  alt={values.comapanyName}
+                  src={values.campany}
+                  style={{
+                    objectFit: "contain",
+                  }}
+                  width={140}
+                  className="p-4"
+                  height={40}
+                />
+              ))}
+            </div>
+          </InfiniteLooper>
+         
+          </div>
+      <div className="lg:w-[50%]">
+          <div className="grid lg:grid-cols-2 gap-10">
+            
+                  <Image src={"/assets/img/1.jpg"} width={800} height={100} alt="anjalicomputer images" className="shadow-2xl rounded-xl"/>
+                  <div>
+                  <Image src={"/assets/img/30.jpg"} width={800} height={100} alt="anjalicomputer images" className="shadow-2xl rounded-xl"/>
+                  <Image src={"/assets/img/18.jpg"} width={800} height={100} alt="anjalicomputer images" className="shadow-2xl rounded-xl mt-6"/>
+                  </div>
+                  <Image src={"/assets/img/1.jpg"} width={300} height={100} alt="anjalicomputer images" className="shadow-2xl rounded-xl -mt-16"/>
+                  <Image src={"/assets/img/24.jpg"} width={300} height={100} alt="anjalicomputer images" className="shadow-2xl rounded-xl m-auto "/>
+                  <Image src={"/assets/img/25.jpg"} width={300} height={100} alt="anjalicomputer images" className="shadow-2xl rounded-xl -mt-[470px] "/>
+
+          </div>
+      </div>
+      </div>
+      </div>
+      </div>
       </div>
     </>
   );
